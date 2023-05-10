8a2e1803305e952a2492cb543dfb29b33779798e
Teten Nugraha <teten.nugraha18@gmail.com>
2023-05-10 13:52:30 +0700

    chore : first endpoint

--- server.js
+++ server.js
@@ -1,17 +1,29 @@
-const express = require('express');
-
-const dotenv = require('dotenv');
-
-
+const express = require("express");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const dotenv = require("dotenv");
 const connectDB = require('./config/db.config');
+// const authRouter = require('./routes/auth-route');
+// const profileRouter = require('./routes/profile-route');
 
 const app = express();
 dotenv.config();
 connectDB();
 
+// MIDDLEWARES
+app.use(bodyParser.json());
+app.use(morgan('tiny'));
+
+
+// ROUTES
+app.get('/', (req, res) => {
+    res.status(200).json("🚀 API is running");
+});
+// app.use('/', authRouter);
+// app.use('/', profileRouter);
+
 // LAUNCH
 const port = process.env.PORT;
-app.listen(5001, () => {
+app.listen(port, () => {
     console.log(`🚀 Server ready at http://0.0.0.0:${port}`);
 });
-
